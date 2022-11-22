99bf0284f27d506692507f0b584758e52f3fa72b
Anthreon <michal.rykalsky@gmail.com>
2022-11-22 15:09:37 +0100

    feature : Added home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
 import "./App.css";
 import EntryScreen from "./screens/EntryScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
-  return <EntryScreen title="Game of Year"></EntryScreen>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen></HomeScreen>}></Route>
+        <Route
+          path="/game"
+          element={<EntryScreen title="Game of Year"></EntryScreen>}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,60 @@
+import { FC, useEffect, useState } from "react";
+import Styles from "./HomeScreen.module.css";
+import { Link } from "react-router-dom";
+import "../assets/fonts/newFont.TTF";
+const background = require("../assets/video/Background.mp4");
+const audio: HTMLAudioElement = new Audio(
+  require("../assets/music/BackgroundMusic.mp4")
+);
+const soundIcon = require("../assets/b.png");
+console.log(soundIcon);
+
+audio.loop = true;
+
+const HomeScreen: FC = () => {
+  const [musicPlaying, setMusicPlaying] = useState(false);
+
+  const playMusic = () => {
+    setMusicPlaying(true);
+    audio.play();
+  };
+
+  const stopMusic = () => {
+    setMusicPlaying(false);
+    audio.pause();
+  };
+
+  const playGame = () => {};
+
+  return (
+    <div>
+      <img
+        onClick={musicPlaying ? stopMusic : playMusic}
+        src={soundIcon}
+        className={`${Styles.soundController} ${
+          musicPlaying ? Styles.musicStopped : Styles.musicPlaying
+        }`}
+      ></img>
+
+      <main>
+        <div className={Styles.menuWrapper}>
+          <Link onClick={stopMusic} className={Styles.startGame} to="/game">
+            <h1>Start Game</h1>
+          </Link>
+        </div>
+
+        <div className={Styles.videoWrapper}>
+          <video
+            className={Styles.video}
+            src={background}
+            autoPlay
+            loop
+            muted
+          />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default HomeScreen;
